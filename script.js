8dbcb69f0d2ad1a510be55c2118fe711ede98e4f
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-04-27 14:43:46 +0000

    pre-loader added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
     var colors = ['#5df0f7', '#42eff7', '#24eaf3', '#bcf6f9'];
 
-    $(".home-container").mousemove(function(event) {
+    $(".home-container").mousemove(function (event) {
         let circle = document.createElement("div");
         circle.setAttribute("class", "circle");
         document.body.appendChild(circle);
@@ -12,11 +12,11 @@ $(document).ready(function() {
         body = eventDoc.body;
 
         circle.style.left = (event.clientX +
-              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-              (doc && doc.clientLeft || body && body.clientLeft || 0)) + 'px';
+            (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+            (doc && doc.clientLeft || body && body.clientLeft || 0)) + 'px';
         circle.style.top = (event.clientY +
-              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-              (doc && doc.clientTop  || body && body.clientTop  || 0 )) + 'px';
+            (doc && doc.scrollTop || body && body.scrollTop || 0) -
+            (doc && doc.clientTop || body && body.clientTop || 0)) + 'px';
 
         let color = colors[Math.floor(Math.random() * colors.length)];
         circle.style.borderColor = color;
@@ -31,26 +31,34 @@ $(document).ready(function() {
         circle.style.borderWidth = "5px";
         circle.style.opacity = 0;
 
-        setTimeout(function() {
+        setTimeout(function () {
             circle.remove();
         }, 500);
     });
-    
+
 });
 
 function sendMail(contactForm) {
-        emailjs.send("service_drsgi1h", "contact_form", {
+    emailjs.send("service_drsgi1h", "contact_form", {
             "from_name": contactForm.name.value,
             "from_email": contactForm.emailaddress.value,
             "customer_request": contactForm.customersummary.value
         })
         .then(
-            function() {
+            function () {
                 window.alert("Thank you for your enquiry."); //alerts the user once they have submitted their enquiry
                 $('#contact-form')[0].reset(); //clear the contact form taken from Stack Overflow (credited in README)
             },
-            function() {
+            function () {
                 window.alert("Failed to submit enquiry.");
             });
-            return false;
-    }
+    return false;
+}
+
+
+window.addEventListener("load", function () {
+            this.setTimeout(function () {
+                        $(".loader").addClass(" hidden");
+                    }, 1000 );
+    
+});
